84eb17681bdccbe37a721a5fcd414af4f64af2c2
jangwonikk <jangwonik@i-nara.co.kr>
2023-10-14 13:38:37 +0900

    commit

---
+++ algorithm/src/test/kotlin/com/github/dhslrl321/algorithm/AlgorithmApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.dhslrl321.algorithm
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AlgorithmApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
